6c298fdff82b3df074350fb1e72cd9ff8b7723d0
Levin Gerdes <gerdeslevin@gmail.com>
2018-02-07 13:59:06 +0100

    Remove broken logging instructions from mesa test script

--- scripts/tests/mesa.rb
+++ scripts/tests/mesa.rb
@@ -18,17 +18,6 @@ Orocos::Process.run 'unit_mesa' do
     Orocos.conf.apply(tofcamera_mesasr, ['hdpr'], :override => true)
     tofcamera_mesasr.configure
 
-    # Log
-    #Orocos.log_all
-    #Orocos.log_all_configuration
-    
-    # Log a single value to a custom lof file
-    logfile = Pocolog::Logfiles.create(File.expand_path('tof_properties', Bundles.log_dir))
-    property = tofcamera_mesasr.property('integration_time')
-    property.log_stream = logfile.create_stream "integration_time", property.type, property.name
-    # Force output of the current properties, otherwise it will only log values when the properties change
-    property.log_current_value
-    
     # Start
     tofcamera_mesasr.start
     
