affdf2134f50def08a48618886cbda5589a6631c
Levin Gerdes <levin.gerdes@esa.int>
2018-03-16 20:13:05 +0100

    Add draft visual odometry script for ExoTeR

--- config/exoter_transformations.rb
+++ config/exoter_transformations.rb
@@ -73,6 +73,10 @@ static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new( -Math::PI/2.0,
 static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new( -Math::PI/2.0, 0.00, -2.0*Math::PI/3.0), 2,1,0),
     Eigen::Vector3.new( 0.195, 0.06, 0.235 ), "left_camera_loccam" => "body"
 
+# transformation used for visual odometry
+static_transform Eigen::Quaternion.from_euler(Eigen::Vector3.new( -Math::PI/2.0, 0.00, -2.0*Math::PI/3.0), 2,1,0),
+    Eigen::Vector3.new( 0.195, 0.06, 0.235 ), "left_camera_viso2" => "body"
+
 # Transformation Left camera to Right camera (Right camera frame expressed in Left camera frame) but transformer expects the other sense
 static_transform Eigen::Quaternion.Identity(),
     Eigen::Vector3.new( 0.12, 0.0, 0.0 ), "right_camera_loccam" => "left_camera_loccam"
