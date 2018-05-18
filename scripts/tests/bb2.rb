9239dceb9bd50523a9295a123a780015a164b2f1
Levin Gerdes <levin.gerdes@esa.int>
2018-05-18 19:16:34 +0200

    Move exoter_visodom.rb from tests to scripts

--- scripts/tests/bb2.rb
+++ scripts/tests/bb2.rb
@@ -20,18 +20,20 @@ Orocos::Process.run 'unit_bb2' do
     camera_bb2 = TaskContext.get 'camera_bb2'
     Orocos.conf.apply(camera_bb2, ['egp_bb2'], :override => true)
     #Orocos.conf.apply(camera_bb2, ['hdpr_bb2'], :override => true)
+    #Orocos.conf.apply(camera_bb2, ['exoter_bb2'], :override => true)
     camera_bb2.configure
-    
+
     stereo_bb2 = TaskContext.get 'stereo_bb2'
     Orocos.conf.apply(stereo_bb2, ['egp_bb2'], :override => true)
     #Orocos.conf.apply(stereo_bb2, ['hdpr_bb2'], :override => true)
+    #Orocos.conf.apply(stereo_bb2, ['exoter_bb2'], :override => true)
     stereo_bb2.configure
 
     # Log
     #Orocos.log_all_ports
     #camera_firewire_bb2.log_all_ports
     #camera_bb2.log_all_ports
-    
+
     # Connect
     camera_firewire_bb2.frame.connect_to camera_bb2.frame_in
     camera_bb2.left_frame.connect_to stereo_bb2.left_frame
@@ -41,7 +43,7 @@ Orocos::Process.run 'unit_bb2' do
     camera_firewire_bb2.start
     camera_bb2.start
     stereo_bb2.start
-    
+
     Readline::readline("Press Enter to exit\n") do
     end
-end 
+end
