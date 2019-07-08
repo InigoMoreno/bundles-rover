a17fce45ca54aac13b88c4715ac6d383d825bec1
Levin Gerdes <levin.gerdes@esa.int>
2019-07-08 14:30:30 +0200

    State during hazard avoidance tests 2019-07-03

--- scripts/tests/unit_hazard_detector.rb
+++ scripts/tests/unit_hazard_detector.rb
@@ -10,7 +10,7 @@ Bundles.initialize
 Orocos::Process.run 'unit_bb2', 'unit_hazard_detector' do
 
     camera_firewire_bb2 = TaskContext.get 'camera_firewire_bb2'
-    Orocos.conf.apply(camera_firewire_bb2, ['exoter_bb2','egp_bb2_id','auto_exposure'], :override => true)
+    Orocos.conf.apply(camera_firewire_bb2, ['hdpr_bb2','egp_bb2_id','auto_exposure'], :override => true)
     camera_firewire_bb2.configure
 
     camera_bb2 = TaskContext.get 'camera_bb2'
