39d9b4db8daae71b481e3cb85b6cadd33e64f66d
Tim Wiese <tim.wiese@esa.int>
2019-07-19 18:07:36 +0200

    Added command arbiter component to exoter_coupled_control

--- scripts/exoter_coupled_control.rb
+++ scripts/exoter_coupled_control.rb
@@ -41,6 +41,10 @@ Orocos.run 'control', 'unit_vicon', 'navigation', 'motion_planning::Task' => 'mo
     Orocos.conf.apply(locomotion_switcher, ['default'], :override => true)
     locomotion_switcher.configure
 
+    command_arbiter = Orocos.name_service.get 'command_arbiter'
+    Orocos.conf.apply(command_arbiter, ['default'], :override => true)
+    command_arbiter.configure
+
 	# setup command joint_dispatcher
     puts "Setting up command joint dispatcher"
     command_joint_dispatcher = Orocos.name_service.get 'command_joint_dispatcher'
@@ -69,7 +73,7 @@ Orocos.run 'control', 'unit_vicon', 'navigation', 'motion_planning::Task' => 'mo
     ptu_control = Orocos.name_service.get 'ptu_control'
     Orocos.conf.apply(ptu_control, ['default'], :override => true)
     ptu_control.configure
-   
+
 	# setup waypoint_navigation
     puts "Setting up waypoint_navigation"
     waypoint_navigation = Orocos.name_service.get 'waypoint_navigation'
@@ -95,51 +99,56 @@ Orocos.run 'control', 'unit_vicon', 'navigation', 'motion_planning::Task' => 'mo
 
     # Connect
     joystick.raw_command.connect_to                       motion_translator.raw_command
+    joystick.raw_command.connect_to                       command_arbiter.raw_command
 
     motion_translator.ptu_command.connect_to              ptu_control.ptu_joints_commands
-    ptu_control.ptu_commands_out.connect_to               command_joint_dispatcher.ptu_commands
+    motion_translator.motion_command.connect_to           command_arbiter.joystick_motion_command
 
-    motion_translator.motion_command.connect_to           locomotion_switcher.motion_command
-    motion_translator.locomotion_mode.connect_to          locomotion_switcher.locomotion_mode_override
+    command_arbiter.motion_command.connect_to             locomotion_switcher.motion_command
+    command_arbiter.locomotion_mode.connect_to            locomotion_switcher.locomotion_mode_override
+
+    ptu_control.ptu_commands_out.connect_to               command_joint_dispatcher.ptu_commands
 
     locomotion_switcher.kill_switch.connect_to            wheel_walking_control.kill_switch
     locomotion_switcher.reset_dep_joints.connect_to       wheel_walking_control.reset_dep_joints
     locomotion_switcher.lc_motion_command.connect_to      locomotion_control.motion_command
-    read_joint_dispatcher.joints_samples.connect_to       locomotion_switcher.joints_readings
-    read_joint_dispatcher.motors_samples.connect_to       locomotion_switcher.motors_readings
-    read_joint_dispatcher.arm_samples.connect_to          coupled_control.currentConfig
-
-    locomotion_control.joints_commands.connect_to         locomotion_switcher.lc_joints_commands
-    wheel_walking_control.joint_commands.connect_to       locomotion_switcher.ww_joints_commands
-
     locomotion_switcher.joints_commands.connect_to        command_joint_dispatcher.joints_commands
 
-    command_joint_dispatcher.arm_commands.connect_to      coupled_control.manipulatorCommand
-    command_joint_dispatcher.motors_commands.connect_to   platform_driver.joints_commands
-    platform_driver.joints_readings.connect_to            read_joint_dispatcher.joints_readings
+    locomotion_control.joints_commands.connect_to         locomotion_switcher.lc_joints_commands
+
+    wheel_walking_control.joint_commands.connect_to       locomotion_switcher.ww_joints_commands
+
+    read_joint_dispatcher.joints_samples.connect_to       locomotion_switcher.joints_readings
+    read_joint_dispatcher.motors_samples.connect_to       locomotion_switcher.motors_readings
     read_joint_dispatcher.motors_samples.connect_to       locomotion_control.joints_readings
+    read_joint_dispatcher.arm_samples.connect_to          coupled_control.currentConfig
     read_joint_dispatcher.joints_samples.connect_to       wheel_walking_control.joint_readings
     read_joint_dispatcher.ptu_samples.connect_to          ptu_control.ptu_samples
 
-   
-    vicon.pose_samples.connect_to                       waypoint_navigation.pose
+    command_joint_dispatcher.motors_commands.connect_to   platform_driver.joints_commands
+
+    platform_driver.joints_readings.connect_to            read_joint_dispatcher.joints_readings
+
+    vicon.pose_samples.connect_to                         waypoint_navigation.pose
 
 	# Motion planning outputs
-	motion_planning.roverPath.connect_to waypoint_navigation.trajectory
-
-	motion_planning.joints.connect_to coupled_control.manipulatorConfig
-	motion_planning.assignment.connect_to coupled_control.assignment
-	motion_planning.sizePath.connect_to coupled_control.sizePath
+	motion_planning.roverPath.connect_to                  waypoint_navigation.trajectory
+	motion_planning.joints.connect_to                     coupled_control.manipulatorConfig
+	motion_planning.assignment.connect_to                 coupled_control.assignment
+	motion_planning.sizePath.connect_to                   coupled_control.sizePath
 
 	# Coupled control outputs
-	coupled_control.modifiedMotionCommand.connect_to locomotion_control.motion_command
+    coupled_control.modifiedMotionCommand.connect_to      command_arbiter.follower_motion_command
+    coupled_control.manipulatorCommand.connect_to         command_joint_dispatcher.arm_commands
 	
 	# Waypoint navigation outputs
-	waypoint_navigation.motion_command.connect_to coupled_control.motionCommand
-	waypoint_navigation.current_segment.connect_to coupled_control.currentSegment
+	waypoint_navigation.motion_command.connect_to         coupled_control.motionCommand
+	waypoint_navigation.current_segment.connect_to        coupled_control.currentSegment
 
     # Start
 	motion_planning.start
+    command_arbiter.start
+	coupled_control.start
     platform_driver.start
     read_joint_dispatcher.start
     command_joint_dispatcher.start
@@ -149,7 +158,6 @@ Orocos.run 'control', 'unit_vicon', 'navigation', 'motion_planning::Task' => 'mo
     ptu_control.start
     motion_translator.start
     joystick.start
-	coupled_control.start
     waypoint_navigation.start
     vicon.start
 
