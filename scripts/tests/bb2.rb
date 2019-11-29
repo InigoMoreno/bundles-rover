6f3d3126d292720b3e3986089d4ccb29b95ca6b6
Matteo De Benedetti <matteo.de.benedetti@esa.int>
2019-11-29 13:19:05 +0100

    added log to bb2.rb script

--- scripts/tests/bb2.rb
+++ scripts/tests/bb2.rb
@@ -14,26 +14,26 @@ Orocos::Process.run 'unit_bb2' do
     # Configure
     camera_firewire_bb2 = TaskContext.get 'camera_firewire_bb2'
     #Orocos.conf.apply(camera_firewire_bb2, ['exoter_bb2','egp_bb2_id'], :override => true)
-    #Orocos.conf.apply(camera_firewire_bb2, ['exoter_bb2'], :override => true)
     Orocos.conf.apply(camera_firewire_bb2, ['exoter_bb2_b'], :override => true)
+    #Orocos.conf.apply(camera_firewire_bb2, ['exoter_bb2_b','auto_exposure'], :override => true)
     camera_firewire_bb2.configure
 
     camera_bb2 = TaskContext.get 'camera_bb2'
     #Orocos.conf.apply(camera_bb2, ['egp_bb2'], :override => true)
-    #Orocos.conf.apply(camera_bb2, ['hdpr_bb2'], :override => true)
+    Orocos.conf.apply(camera_bb2, ['hdpr_bb2'], :override => true)
     #Orocos.conf.apply(camera_bb2, ['exoter_bb2'], :override => true)
-    Orocos.conf.apply(camera_bb2, ['test_hdpr_bb2'], :override => true)
+    #Orocos.conf.apply(camera_bb2, ['test_hdpr_bb2'], :override => true)
     camera_bb2.configure
 
     stereo_bb2 = TaskContext.get 'stereo_bb2'
     #Orocos.conf.apply(stereo_bb2, ['egp_bb2'], :override => true)
-    ## Orocos.conf.apply(stereo_bb2, ['hdpr_bb2'], :override => true)
+    # Orocos.conf.apply(stereo_bb2, ['hdpr_bb2'], :override => true)
     #Orocos.conf.apply(stereo_bb2, ['exoter_bb2'], :override => true)
     Orocos.conf.apply(stereo_bb2, ['test_hdpr_bb2'], :override => true)
     stereo_bb2.configure
 
     # Log
-    #Orocos.log_all_ports
+    Orocos.log_all_ports
     #camera_firewire_bb2.log_all_ports
     #camera_bb2.log_all_ports
 
