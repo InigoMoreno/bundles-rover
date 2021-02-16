0b7848afad687cb944e2ccb80ae3ebde21990e71
Maximilian Stoelzle <maximilian@stoelzle.ch>
2021-02-16 13:02:57 +0000

    Add Valley circle track

--- scripts/replay/tenerife_log_dems_viz.rb
+++ scripts/replay/tenerife_log_dems_viz.rb
@@ -41,6 +41,10 @@ bag = Orocos::Log::Replay.open(
         '/media/heimdal/Dataset1/9June/Traverse/20170609-1909/bb3.log',
         '/media/heimdal/Dataset1/9June/Traverse/20170609-1909/waypoint_navigation.log',
         '/media/heimdal/Dataset1/9June/Traverse/20170609-1909/imu.log',
+#       Valley Circle
+#        '/media/heimdal/Dataset1/11June/Traverse/20170611-1407/bb3.log',
+#        '/media/heimdal/Dataset1/11June/Traverse/20170611-1407/waypoint_navigation.log',
+#        '/media/heimdal/Dataset1/11June/Traverse/20170611-1407/imu.log',
 )
 bag.use_sample_time = true
 
